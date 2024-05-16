b67528eeb6aa868dbb7a27bcc44616e1c8915ea8
makenasandra <makenawachira@gmail.com>
2024-05-16 09:45:04 +0300

    added challenge

--- codewars/calculating-with-functions.js
+++ codewars/calculating-with-functions.js
@@ -18,125 +18,85 @@ https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
  */
 
 //My Solution
+function calculate(num, op) {
+  if (!op) return num;
+  switch (op["operator"]) {
+    case "*":
+      return num * op["operand"];
+      break;
+    case "-":
+      return num - op["operand"];
+      break;
+    case "+":
+      return num + op["operand"];
+      break;
+    case "/":
+      return Math.trunc(num / op["operand"]);
+      break;
+  }
+}
 function zero(op) {
-    let result;
-    const num = 0;
-    
-    if(!op) return num;
-    switch (op["operator"]){
-        case "*": return num*op["multiplier"] 
-        break;
-        case "-": return num-op["multiplier"] 
-        break;
-        case "+": return num+op["multiplier"] 
-        break;
-        case "/": return num/op["multiplier"] 
-        break;
-    }
-  }
-  function one(op) {
-     const num = 1;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function two(op) {
-     const num = 2;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function three(op) {
-     const num = 3;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function four(op) {
-    const num = 4;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function five(op) {
-    const num = 5;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : "";
-  }
-  function six(op) {
-     const num = 6;
-    if(!op) return num;
-    console.log(op["operator"]==="*");
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function seven(op) {
-    if(!op) return 7;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? 7*op["multiplier"]
-    : op["operator"]==="-"? 7-op["multiplier"]
-    : op["operator"]==="+"? 7+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(7/op["multiplier"])
-    : ""
-  }
-  function eight(op) {
-     const num = 8;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  function nine(op) {
-     const num = 9;
-    if(!op) return num;
-    console.log(op["operator"]==="*")
-    return op["operator"]==="*"? num*op["multiplier"]
-    : op["operator"]==="-"? num-op["multiplier"]
-    : op["operator"]==="+"? num+op["multiplier"]
-    : op["operator"]==="/"? Math.trunc(num/op["multiplier"])
-    : ""
-  }
-  
-  function plus(num) {
-     let op = {operator: "+", multiplier: num }
-    return op;
-  }
-  function minus(num) {
-    let op = {operator: "-", multiplier: num }
-    return op;
-  }
-  function times(num) {
-     let op = {operator: "*", multiplier: num }
-    return op;
-  }
-  function dividedBy(num) {
-     let op = {operator: "/", multiplier: num }
-    return op;
-  }
+  return calculate(0, op);
+}
+
+function one(op) {
+  return calculate(1, op);
+}
+function two(op) {
+  return calculate(2, op);
+}
+function three(op) {
+  return calculate(3, op);
+}
+function four(op) {
+  return calculate(4, op);
+}
+function five(op) {
+  return calculate(5, op);
+}
+function six(op) {
+  return calculate(6, op);
+}
+function seven(op) {
+  return calculate(7, op);
+}
+function eight(op) {
+  return calculate(8, op);
+}
+function nine(op) {
+  return calculate(9, op);
+}
+
+const operation = (operator, operand) => {
+  return { operator, operand };
+};
+
+function plus(num) {
+  return operation("+", num);
+}
+function minus(num) {
+  return operation("-", num);
+}
+function times(num) {
+  return operation("*", num);
+}
+function dividedBy(num) {
+  return operation("/", num);
+}
+
+//Best SOlution
+const zero = a => a ? a(0) : 0
+const one = a => a ? a(1) : 1
+const two = a => a ? a(2) : 2
+const three = a => a ? a(3) : 3
+const four = a => a ? a(4) : 4
+const five = a => a ? a(5) : 5
+const six = a => a ? a(6) : 6
+const seven = a => a ? a(7) : 7
+const eight = a => a ? a(8) : 8
+const nine = a => a ? a(9) : 9
+
+const plus = a => b => a + b
+const minus = a => b => b - a
+const dividedBy = a => b => Math.floor(b / a)
+const times = a => b => a * b
